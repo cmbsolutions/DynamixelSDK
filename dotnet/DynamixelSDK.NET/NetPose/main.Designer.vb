cdc8686c59e1713e39e61f15a7a0849c5df2d2d2
Maurice Beckman <maurice.beckman@gmail.com>
2018-08-22 23:02:10 +0200

    first crack @ NetPose

---
+++ dotnet/DynamixelSDK.NET/NetPose/main.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Legassembly1 = New Assemblies.legassembly()
+        Me.Legassembly2 = New Assemblies.legassembly()
+        Me.Legassembly3 = New Assemblies.legassembly()
+        Me.Legassembly4 = New Assemblies.legassembly()
+        Me.Legassembly5 = New Assemblies.legassembly()
+        Me.Legassembly6 = New Assemblies.legassembly()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Legassembly1
+        '
+        Me.Legassembly1.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange
+        Me.Legassembly1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Legassembly1.Location = New System.Drawing.Point(2, 1)
+        Me.Legassembly1.Name = "Legassembly1"
+        Me.Legassembly1.Size = New System.Drawing.Size(361, 270)
+        Me.Legassembly1.TabIndex = 1
+        '
+        'Legassembly2
+        '
+        Me.Legassembly2.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange
+        Me.Legassembly2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Legassembly2.Location = New System.Drawing.Point(362, 1)
+        Me.Legassembly2.Name = "Legassembly2"
+        Me.Legassembly2.Size = New System.Drawing.Size(361, 270)
+        Me.Legassembly2.TabIndex = 2
+        '
+        'Legassembly3
+        '
+        Me.Legassembly3.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange
+        Me.Legassembly3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Legassembly3.Location = New System.Drawing.Point(362, 270)
+        Me.Legassembly3.Name = "Legassembly3"
+        Me.Legassembly3.Size = New System.Drawing.Size(361, 270)
+        Me.Legassembly3.TabIndex = 4
+        '
+        'Legassembly4
+        '
+        Me.Legassembly4.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange
+        Me.Legassembly4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Legassembly4.Location = New System.Drawing.Point(2, 270)
+        Me.Legassembly4.Name = "Legassembly4"
+        Me.Legassembly4.Size = New System.Drawing.Size(361, 270)
+        Me.Legassembly4.TabIndex = 3
+        '
+        'Legassembly5
+        '
+        Me.Legassembly5.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange
+        Me.Legassembly5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Legassembly5.Location = New System.Drawing.Point(722, 270)
+        Me.Legassembly5.Name = "Legassembly5"
+        Me.Legassembly5.Size = New System.Drawing.Size(361, 270)
+        Me.Legassembly5.TabIndex = 6
+        '
+        'Legassembly6
+        '
+        Me.Legassembly6.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange
+        Me.Legassembly6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Legassembly6.Location = New System.Drawing.Point(722, 1)
+        Me.Legassembly6.Name = "Legassembly6"
+        Me.Legassembly6.Size = New System.Drawing.Size(361, 270)
+        Me.Legassembly6.TabIndex = 5
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(1198, 1)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(32, 300)
+        Me.Panel1.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Image = Global.NetPose.My.Resources.Resources.control_rewind_blue1
+        Me.Button1.Location = New System.Drawing.Point(4, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(23, 25)
+        Me.Button1.TabIndex = 8
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.PictureBox1.ErrorImage = Nothing
+        Me.PictureBox1.Image = Global.NetPose.My.Resources.Resources.servonum
+        Me.PictureBox1.InitialImage = Nothing
+        Me.PictureBox1.Location = New System.Drawing.Point(33, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(458, 299)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1231, 551)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Legassembly5)
+        Me.Controls.Add(Me.Legassembly6)
+        Me.Controls.Add(Me.Legassembly3)
+        Me.Controls.Add(Me.Legassembly4)
+        Me.Controls.Add(Me.Legassembly2)
+        Me.Controls.Add(Me.Legassembly1)
+        Me.DoubleBuffered = True
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "main"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "NetPose"
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Legassembly1 As Assemblies.legassembly
+    Friend WithEvents Legassembly2 As Assemblies.legassembly
+    Friend WithEvents Legassembly3 As Assemblies.legassembly
+    Friend WithEvents Legassembly4 As Assemblies.legassembly
+    Friend WithEvents Legassembly5 As Assemblies.legassembly
+    Friend WithEvents Legassembly6 As Assemblies.legassembly
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+End Class
---
+++ dotnet/DynamixelSDK.NET/NetPose/main.vb
@@ -0,0 +1,11 @@
+﻿Public Class main
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Panel1.Width = 32 Then
+            Panel1.Width = 492
+            Panel1.Left = Width - 17 - Panel1.Width
+        Else
+            Panel1.Width = 32
+            Panel1.Left = Width - 17 - Panel1.Width
+        End If
+    End Sub
+End Class
